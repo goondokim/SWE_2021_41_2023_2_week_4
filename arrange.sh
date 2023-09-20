72bbd91b97720ee1cb70af905dca8e964894a8ea
goondokim <143521925+goondokim@users.noreply.github.com>
2023-09-20 21:12:18 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,14 @@
+
+
+for i in "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"
+do mv ./files/$i* ./$i
+done 
+list = ("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
+
+for i in ./files
+do f = i | cut -c 1
+for k in list
+do if [f=k]
+then mv ./files/$i ./$k
+done
+done
